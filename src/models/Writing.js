b3dc72ea26615c748201b5fe4c665374c8ff4e5d
lu15v <lu15bytes@gmail.com>
2021-03-17 19:54:29 -0700

    FIX: ISO String to writings

--- src/models/Writing.js
+++ src/models/Writing.js
@@ -19,7 +19,7 @@ const writingSchema = new Schema({
     },
     createdAt:{
         type: Date,
-        default: Date.now
+        default: new Date().toISOString()
     },
     likes: [
         {
